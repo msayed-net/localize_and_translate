2d9bfe479abb86a558bcee554751247ed4a6c495
Mohamed Sayed <msayed@macs-MacBook-Air.local>
2019-12-31 16:43:35 +0200

    beta version almost ready

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:localize_and_translate/localize_and_translate.dart';
+import 'package:print_color/print_color.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+
+  // if your flutter > 1.7.8 
+  // ensure flutter activated
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  LIST_OF_LANGS = ['ar', 'en']; // define languages
+  LANGS_DIR = 'assets/langs/'; // define directory
+  await translator.init(); // intialize
+
+  runApp(
+    LocalizedApp(
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,6 +27,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   void initState() {
+    Print.green('starting'); // print_color plugin
     super.initState();
   }
 
@@ -18,15 +35,37 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        drawer: Drawer(),
         appBar: AppBar(
-          title: const Text('print_color'),
+          title: Text(translator.translate('appTitle')),
+          // centerTitle: true,
         ),
-        body: Center(
-            child: OutlineButton(
-          onPressed: () {
-          },
-          child: Text('Click Me For Magic'),
-        )),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 50),
+              Text(
+                translator.translate('textArea'),
+                // textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 35),
+              ),
+              SizedBox(height: 150),
+              OutlineButton(
+                onPressed: () {
+                  translator.setNewLanguage(
+                    context,
+                    newLanguage: translator.currentLanguage == 'ar' ? 'en' : 'ar',
+                    remember: true,
+                    restart: true,
+                  );
+                },
+                child: Text(translator.translate('buttonTitle')),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/localize_and_translate.dart
+++ lib/localize_and_translate.dart
@@ -1,17 +1,113 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+List<String> LIST_OF_LANGS = [];
+String LANGS_DIR;
 
 class LocalizeAndTranslate {
-  
+  // ---- Config ---- //
+  Locale _locale;
+  Map<dynamic, dynamic> _values;
+
+  ///------------------------------------------------
+  /// Locals List
+  ///------------------------------------------------
+  Iterable<Locale> locals() => LIST_OF_LANGS.map<Locale>(
+        (lang) => new Locale(lang, ''),
+      );
+
+  ///------------------------------------------------
+  /// Transle : [key]
+  ///------------------------------------------------
+  String translate(String key) {
+    return (_values == null || _values[key] == null)
+        ? '404 : $key'
+        : _values[key];
+  }
+
+  get currentLanguage =>
+      _locale == null ? LIST_OF_LANGS[0] : _locale.languageCode;
+
+  Future<Null> init([String lang]) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (_locale == null) {
+      if (prefs.getString('currentLang') != null) {
+        _locale = Locale(prefs.getString('currentLang'), "");
+      }
+      await setNewLanguage(lang, restart: false);
+    }
+    return null;
+  }
+
+  Future<Null> setNewLanguage(
+    context, {
+    @required bool restart,
+    String newLanguage,
+    bool remember = true,
+  }) async {
+    if (newLanguage == null || newLanguage == "") {
+      newLanguage = _locale == null ? LIST_OF_LANGS[0] : _locale.languageCode;
+    }
+
+    _locale = Locale(newLanguage, "");
+
+    String content = await rootBundle.loadString(
+      LANGS_DIR + "$newLanguage.json",
+    );
+    _values = json.decode(content);
+
+    if (remember) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('currentLang', newLanguage);
+    }
+
+    if (restart) {
+      LocalizedApp.restart(context);
+    }
+
+    return null;
+  }
+
+  restart(context) {
+    LocalizedApp.restart(context);
+  }
 }
 
-class LocalizedApp extends StatefulWidget{
+LocalizeAndTranslate translator = new LocalizeAndTranslate();
+
+class LocalizedApp extends StatefulWidget {
+  final Widget child;
+
+  LocalizedApp({this.child});
+
+  static restart(BuildContext context) {
+    final _LocalizedAppState state = context.ancestorStateOfType(
+      const TypeMatcher<_LocalizedAppState>(),
+    );
+    state.restart();
+  }
+
   @override
   _LocalizedAppState createState() => _LocalizedAppState();
 }
 
 class _LocalizedAppState extends State<LocalizedApp> {
+  Key key = new UniqueKey();
+
+  void restart() {
+    this.setState(() {
+      key = new UniqueKey();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return new Container(
+      key: key,
+      child: widget.child,
+    );
   }
 }
