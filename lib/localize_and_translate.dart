8af48473efb79089ad3a114b937465e44c06465a
Mohamed Sayed <msayed95@icloud.com>
2023-09-03 22:34:54 +0300

    hotfix - export enums

--- lib/localize_and_translate.dart
+++ lib/localize_and_translate.dart
@@ -1,7 +1,7 @@
 library localize_and_translate;
 
-export 'package:intl/intl.dart';
 export 'package:localize_and_translate/src/assets/asset_loader_root_bundle_json.dart';
+export 'package:localize_and_translate/src/constants/enums.dart';
 export 'package:localize_and_translate/src/core/localize_and_translate.dart';
 export 'package:localize_and_translate/src/extensions/context_extensions.dart';
 export 'package:localize_and_translate/src/extensions/string_extensions.dart';
